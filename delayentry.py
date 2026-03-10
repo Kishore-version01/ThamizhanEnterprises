411576f30345d157e8a3b682d1f24100bea8e1e7
SirThamizhan <kk1416307@gmail.com>
2026-03-10 21:08:00 +0530

    Aided delay delay entry verion-0.01

--- delayentry.py
+++ delayentry.py
@@ -1,5 +1,5 @@
 from supabase import create_client, Client
-import string 
+import string
 import os
 
 
@@ -14,17 +14,31 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 class Delayentry:
+    def route_dropdown(self, train_no):
+        response = supabase.table("Route_data").select("stop_seq,distance_frm,station,sched_arr,sched_dep").eq("train_no",train_no).order("stop_seq").execute()
+        for row in response.data:
+            print(row["station"], row["stop_seq"])
+        return response.data
+
     def delay_train(self, train_no, date, stations, arr_timings, dep_timings, holiday_flags, weathers):
         trip_id=str(train_no)+str(date)
+        route_data = self.route_dropdown(train_no)
+
+        rows=[]
         
-        row=[]
         for i in range(len(stations)):
-            row.append({
-                "trip_id":trip_id
+            rows.append({
+                "trip_id" : trip_id,
+                "date":date,
                 "train_no":train_no,
-                "station":stations[i]
-                "actual_arr":arr_timings[i]
-                "actual_dep":dep_timings[i]
-                "holiday_flag": holiday_flags[i]
+                "stop_seq":route_data["stop_seq"],
+                "station":stations[i],
+                "distance_frm_src":route_data["distance_frm"],
+                "sched_arr":route_data["sched_arr"],
+                "sched_dep":route_data["sched_dep"],
+                "actual_arr":arr_timings[i],
+                "actual_dep":dep_timings[i],
+                "holiday_flag": holiday_flags[i],
                 "weather":weathers[i]
                 })
+        supabase.table("Delay_format").insert(rows).execute()
